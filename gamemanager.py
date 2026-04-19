dfc47140d5c2d14eba3d1449ce69df2d28234f8f
z00mm00z <rvanwoud@icloud.com>
2026-04-19 15:44:03 +0200

    Cleanup and Comments

--- gamemanager.py
+++ gamemanager.py
@@ -16,43 +16,56 @@ import gameover
 
 import constants
 import bunkers
-class GamePlay:#class that defines the current instance of the game
+class GameManager: #class that defines the current instance of the game
     def __init__(self,vAlien,tally):
         self.vAlien = vAlien#speed of the alien
         self.tally = tally#score
 
     def play_game(self):
+
+        # -- INITIALISE
+
         game_over = gameover.GameOver()#create a GameOver object 
         time_shot = clock.Clock(0) #create a clock object to track the time between bullets shot
         time = clock.Clock(0) #create a clock object to track the game time
+
         x = 0.5#starting x position for shooter
         vx =0 #starting velocity of shooter
         a = math.pi/2 #starting angle of turret
         av =0 #starting angular velocity of turret
+
         bullet =[] #create an array of bullet objects as an empty array
         player_one = shooter.Shooter(x,vx,a,av,bullet,time_shot) #create a Shooter object with the initial values specified above
         xAlien = 0.1 #starting alien position
         yAlien = 0.9 #starting alien position
         vxAlien = self.vAlien  #set the initial alien velocity to that of the game instance
         aliens_arr = [] #inintialise an empty array to store the aliens
+
         for i in range(12): #initialise 24 aliens in two rows
             aliens_arr += [aliens.Aliens(xAlien + constants.ALIEN_HITBOX*(i+1),yAlien,vxAlien,bullet,False,0.55-constants.ALIEN_HITBOX*i,constants.ALIEN_HITBOX*i,player_one,self.tally,time,game_over)]
         for i in range(12):
             aliens_arr += [aliens.Aliens(xAlien+ constants.ALIEN_HITBOX*(i+1),yAlien - constants.ALIEN_HITBOX,vxAlien,bullet,False,0.55-constants.ALIEN_HITBOX*i,constants.ALIEN_HITBOX*i,player_one,self.tally,time,game_over)]
+        
         blown = False #**
         random_alien = aliens_arr[random.randrange(0,24)] #choose a random alien
         bomb = bombs.Bomb(random_alien.x,random_alien.y,player_one.x,constants.SHOOTER_Y,game_over) #create a bomb object 
         bunker1= bunkers.Bunker(0.2,0.4,3,bullet,bomb,aliens_arr)
         bunker2 = bunkers.Bunker(0.8,0.4,3,bullet,bomb,aliens_arr)
-        while True: #keep the game running
+
+        # Manage The Game Loop, Run until we reach an end condition
+        
+        while True:
+
             time.updateTime() #add to the time for the game time
             time_shot.updateTime() #add to the time for the time between shots
             stddraw.clear(stddraw.GREEN) #set the canvas to green
             bunker1.update_bunker()
             bunker2.update_bunker()
+
             self.tally.update_score()#update the score board
             bomb_hit = bomb.bomb_update()#update the bombs
             stddraw.text(0.1,0.9,"Lives: " +str(game_over.lives)) #draw the lives i nthe top right hand corner
+
             all_aliens_destroyed = True
             for i in aliens_arr: # check if all the aliens are dead
                 all_aliens_destroyed = all_aliens_destroyed and i.blown
@@ -64,25 +77,29 @@ class GamePlay:#class that defines the current instance of the game
                bomb.y =random_alien.y
                bomb.x = random_alien.x
             bomb.x_shooter = player_one.x #update the x position of the shooter to the bomb 
+
+            # Update Game State
+
             if player_one.update_shooter(): #if the shooter method that update the shooter returns true return "end"
-                return "end"
+                return "game_quit"
             if all_aliens_destroyed: #if all aliens are dead end the level by returning "endLevel"
-                return "endLevel"
+                return "level_end" 
             if bomb_hit:
                 if game_over.update_game_over():
                     game_over.end_game()
                     stddraw.show(1000)
-                    return "gameover"
+                    return "game_over"
             for i in aliens_arr: #check all of the aliens to see if they have killed the player
                if i.update_alien(): #has an alien killed the player
                    if game_over.update_game_over():
                        game_over.end_game()
                        stddraw.show(1000)
-                       return "gameover"
+                       return "game_over"
+            
+            
 
             stddraw.show(10)#display the game
 
-def main():
-    playGame()
 
-if __name__ == "__main__": main()
+    def run_loop():
+        pass
--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import stddraw  # type: ignore
 import stdaudio  # type: ignore
-import stdio  # type: ignore
 import titlescreen
-import gameplay
+import gamemanager
 import threading
 import highscore
 import score
@@ -23,15 +22,28 @@ class Music:
 
 
 def main():
-    stddraw.setCanvasSize(1000, 1000)  # Set the canvas size to 1000 x 1000
+
+    # -- INITIALISATION
+
+    # Score and State Related Variables
     score_manager = highscore.highScore()  # Inititalise Score Manager
     tally = score.ScoreBoard()  # create a ScoreBoard object
+    level = 0 
+    
+    # Canvas
+    stddraw.setCanvasSize(1000, 1000)  # Set the canvas size to 1000 x 1000   
     titlescreen.displayTitleScreen()  # set the title screen on the canvas
     stddraw.setPenColor(stddraw.BLACK)  # change pen color to black
-    tunes = Music()  # create a music object
+    
+    # Create Music Object and Thread Music To Play While Program Executes
+    tunes = Music() 
     threading.Thread(
         target=tunes.play_music, daemon=True
-    ).start()  # thread the music to play while the rest of the program executes
+    ).start() 
+
+    # -- START GAME
+
+    # Wait For The Initial Key Press To Start The Game
     while True:
         if stddraw.hasNextKeyTyped():  # check if a key is pressed
             key = stddraw.nextKeyTyped()
@@ -40,36 +52,48 @@ def main():
             break  # end the title screen loop
         stddraw.show(10)  # keep displaying the title screen
 
-    vAlien = 0.0005  # set the starting alien speed
-    game = gameplay.GamePlay(
+    # Create a gameplay object, pass in inital alient velocity and tally object parameters
+    vAlien = 0.0005
+    game = gamemanager.GameManager(
         vAlien, tally
-    )  # create a gameplay object with the initial alien speed and score
-    game_status = game.play_game()  # play the game
-    level = 0  # set level to 0
-    while True:  # game loop
-        if game_status == "endLevel":  # if the .playGame() method returned "endlevel"
+    )
+    
+
+    ## -- GAME LOOP
+
+    while True: 
+
+        # Run The Next Game Loop, Store The New Game State
+        game_state = gamemanager.play_game() 
+
+        if game_state == "level_end":  # if the .playGame() method returned "endlevel" # TODO level_end
+
+            # The level has ended. Adjust parameters like level value and alien speed.
+
             vAlien = vAlien + 0.0025  # increase the speed of the alien
             game.vAlien = vAlien  # update the value for the object
             level += 1  # increase the level
+
+            # TODO Get this display changing out of here and somewhere else
+            
             stddraw.setFontSize(50)  # change the font size to 50
             stddraw.text(
                 0.5, 0.5, "level " + str(level)
             )  # Display the level in the middle of the screen
             stddraw.show(1000)  # wait a second to dispaly the level
             stddraw.setFontSize(18)  # reset the font size
-        else:
-            if (
-                game_status == "gameover"
-            ):  # if the .gamePlay() method returned "gameover"
-                game.vAlien = 0.0005  # reset the alien speed
-                stddraw.setPenColor(stddraw.BLACK)  # reset the pen color
-                stddraw.setFontSize(18)  # reset the font size
-                level = 0  # reset the level
-                tally.score = 0  # reset the score
-            else:
-                if game_status == "end":  # if the .gamePlay() method returned "end"
-                    break  # end the game
-        game_status = game.play_game()  # play the game again
+
+        elif game_state == "game_over": # TODO game_over
+            # if the .gamePlay() method returned "gameover"
+            game.vAlien = 0.0005  # reset the alien speed
+            stddraw.setPenColor(stddraw.BLACK)  # reset the pen color
+            stddraw.setFontSize(18)  # reset the font size
+            level = 0  # reset the level
+            tally.score = 0  # reset the score
+
+        # If the game has ended, exit the game loop
+        elif game_state == "game_quit": # if the .gamePlay() method returned "end" # TODO game_quit
+            break
 
 
 if __name__ == "__main__": main()
